b1950e81d867402a9419686b3de14bd0beb49662
totrung92 <totrung92@gmail.com>
2025-08-24 15:16:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const express = require("express");
+const { Pool } = require("pg");   // 👈 thêm dòng này
+
+const app = express();
+app.use(express.json());
+
+// Cấu hình kết nối Postgres
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }   // Render yêu cầu SSL
 });
